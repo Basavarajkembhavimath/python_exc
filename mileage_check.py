56fffcc31097ac9faaf4de2e4c291c300f4748d4
Basavarajkembhavimath <55747930+Basavarajkembhavimath@users.noreply.github.com>
2019-09-26 17:18:03 +0530

    create mileage_check.py

---
+++ mileage_check.py
@@ -0,0 +1,16 @@
+# python_exc
+
+
+"""using the starting and ending values of cars odometer
+and measure the mileage"""
+
+
+
+
+starting_value = 0
+ending_value = 500
+fule_consuption = 30
+distance = ending_value - starting_value
+mileage = distance//fule_consuption
+
+print ("your cars mileage is",mileage)
