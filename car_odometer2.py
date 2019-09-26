c16aab1692870b1683a54bd27fc32d44c4622677
Basavarajkembhavimath <55747930+Basavarajkembhavimath@users.noreply.github.com>
2019-09-26 17:24:55 +0530

    create car_odometer2.py

---
+++ car_odometer2.py
@@ -0,0 +1,13 @@
+# python_exc
+
+
+distance = 560
+starting_value = 0
+ending_value = 560
+tank_capacity = 20
+mileage = 8
+fule_conpsumation = (ending_value - starting_value)/mileage
+num_of_stops = fule_conpsumation/tank_capacity
+print ("fule_conpsumation",fule_conpsumation)
+print ("num_of_stops",num_of_stops)
+
