1ad3e6ade2ba77ae37e29fe11607aaad74657bfa
Basavarajkembhavimath <55747930+Basavarajkembhavimath@users.noreply.github.com>
2019-09-26 16:09:40 +0530

    create comment

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+# python_exc
+"""
+a = ('hello world')
+b = ('hello world')
+c = ('hello world')
+d = ('hello world')
+e = ('hello world')
+print (a)
+print (b)
+print (c)
+print (a,e)
+
+n = ('hello world')
+c = 0
+while  c <= 9:
+    print (n)
+    c= c+1
+"""  
+n= "hello world"
+for i in range(5):
+    print(n,end='\n')
