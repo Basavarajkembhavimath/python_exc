17c0bb148f0fc27505d14e254b53de466468f0f4
Basavarajkembhavimath <55747930+Basavarajkembhavimath@users.noreply.github.com>
2019-10-03 16:46:20 +0530

    creat_mul_any_num_fun

---
+++ mul_any_num_fun.py
@@ -0,0 +1,29 @@
+
+"""
+S01AQ03
+     - Modify program in S01Q02 to print the multiplication table 
+           of any number desired by the user
+
+"""
+
+def user_input():
+    x = int(input("enter your multiplication number"))
+    return (x)
+
+def multiplication(a, b):
+    return (a * b)
+
+def main():
+    m = user_input()
+    for i in range(1, 11):
+        n = i
+        y = multiplication(m, i)
+        print("your multplication number is =", m, "x", n, "=", y)
+
+
+#main start here
+main()
+
+
+
+
