0bb2a5e461125bba612f2c7d3110449ea828b0f0
Basavarajkembhavimath <55747930+Basavarajkembhavimath@users.noreply.github.com>
2021-01-13 12:09:11 +0530

    Update multipliction.py
    Updated the code

--- multipliction.py
+++ multipliction.py
@@ -1,7 +1,9 @@
-n=17
-c = 1
-print (n)
+n = 17
+c = 0
 while  c<=9:
     c = c+1
     b = n*c
-    print (b)
+    x = str(n)+" X "+str(c)+" "
+    if len(str(c)) > 1:
+    	x = str(n)+" X "+str(c)
+    print (x,"=",b)
