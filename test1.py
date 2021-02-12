1793e7f624bdb16039cd113c5b7799f32dd735e5
Basavarajkembhavimath <55747930+Basavarajkembhavimath@users.noreply.github.com>
2021-02-12 16:25:35 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -28,6 +28,7 @@ with open("first_test.txt") as file:
         temp3   = []
         for p_num, pag in enumerate(file[:6], 1):
             page = get_item(pag)
+            p_len = len(temp) + len(temp2)
             pages.append(page)
             if len(query) == 1 :
                 if query[0] in page:
@@ -37,44 +38,58 @@ with open("first_test.txt") as file:
                         temp.append(p)
                     else:
                         p = 'p'+str(p_num)
-                        if p not in temp:
-                            temp.append(p)
+                        if p not in temp2:
+                            temp2.append(p)
 
             if len(query) >= 2 :
                 if query[0] in page and query[1] in page :
                     p = 'p'+str(p_num)
+                    #print("and",query)
                     if len(page) == 2:
-                        if p not in temp:
-                            temp.append(p)
+                        temp.append(p)
+                        #print("if",page,temp)
                     else:
-                        if p not in temp:
-                            temp.append(p)
+                        if p not in temp and p not in temp2:
+                            temp2.append(p)
+                            #print("else",page,temp2)
                         
-                if query[0] in page:
+                if query[0] in page :
                     idx = get_idx(page, query[0])
+                    #print("and",query)
                     if idx == 0:
                         p = 'p'+str(p_num)
-                        if p not in temp:
+                        if p not in temp and p not in temp2:
                             temp.append(p)
+                            #print("q 0 if",page,temp)
                     else:
                         p = 'p'+str(p_num)
-                        if p not in temp:
-                            temp.append(p)
+                        if p not in temp and p not in temp2:
+                            temp2.append(p)
+                            #print("q 0 else",page,temp2)
 
                 if query[1] in page:
                     idx = get_idx(page, query[1])
-                    if idx >= 1 :
-                        p = 'p'+str(p_num)
-                        if p not in temp:
-                            temp.append(p)
+                    p = 'p'+str(p_num)
+                    if idx == 0:
+                        if p not in temp and p not in temp2:
+                            temp2.append(p)
+                            #print("q 1 if",page,temp)
                     else:
                         p = 'p'+str(p_num)
-                        if p not in temp:
-                            temp.append(p)
-
-        Qr['Q'+str(q_num)] = temp+temp2+temp3
-al = 'a b'
-print(Qr)
-name = iter(pages)
-print(next(name))
-print(str((al)))
+                        if p not in temp and p not in temp2:
+                            temp3.append(p)
+                            #print("q 1 else",page,temp3)
+                    
+        p_len = len(temp) + len(temp2)+len(temp3)
+        my_list = temp+temp2+temp3
+        if p_len <= 5:
+            Qr['Q'+str(q_num)] = my_list
+        else:
+            my_list.pop(-1)
+            Qr['Q'+str(q_num)] = my_list
+        
+for qr, pg in Qr.items():
+    print(qr+':',end = ' ')
+    for item in pg:
+        print(item, end = ' ')
+    print('',end='\n')
